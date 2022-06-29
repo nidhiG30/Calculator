b3a13405deef1ccb3d08f80dcde95c04d310914f
Nidhi Gadge <86159434+nidhiG30@users.noreply.github.com>
2022-06-29 21:39:28 +0530

    Add files via upload
    Basic first impressions of the project.

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let result = document.querySelector('.result'); // input/output display
+let operators = document.querySelector('operator'); // operator buttons
+let clearAll = document.querySelector('.clear-all'); // Clear All Previous Data
+let numPad = document.querySelectorAll('num'); // Number pad
+let equal = document.querySelector('.equal'); // Equal sign for calculating result
